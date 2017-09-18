9d5fdf998151c97bcb88b6f433636fe76c4d306e
rahul <RAhul.excel2011@gmail.com>
2017-09-18 18:53:39 +0530

    registration form and api

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 var express =require('express');
 const router=express.Router();
-const Contact=require('../models/collection')
+const Contact=require('../models/collection');
+const Register=require('../models/register');
+const validation=require('../validation/registervalidation');
 router.get('/contacts',(req,res,next)=>{
 	Contact.find((err,contacts)=>{
 		res.json(contacts);
@@ -15,6 +17,7 @@ router.post('/addcontact',(req,res,next)=>{
 	});
 	newContact.save((err,contact)=>{
 		if(err){
+			 return next(err);
 			res.json({msg:'failed to update data'});
 		}
 		else{
@@ -22,5 +25,32 @@ router.post('/addcontact',(req,res,next)=>{
 		}
 	});
 });
+router.post('/register',(req,res,next)=>{
+	validation.validateRegistration(req.body, function(err, data){
+		if (err) {
+            next(err);
+        } else {
+        	let newUser= new Register({
+		email:req.body.email,
+		username:req.body.username,
+		password:req.body.password,
+		confirm_password:req.body.confirmpassword,
+
+
+	});
+	newUser.save((err,contact)=>{
+		if(err){
+			 return next(err)
+			res.json({msg:'failed to update data'});
+		}
+		else{
+			res.json({msg:'contact created'});
+		}
+	})
+}
+	})
+	
+});
+
 module.exports=router;
 
